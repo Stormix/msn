4f584c8d1440a97dc7f77bfa31d9d2479c17a6cd
Anas Mazouni <madadj4@gmail.com>
2023-11-10 22:28:26 +0100

    NO FOOTER

--- client/src/components/template/layout.tsx
+++ client/src/components/template/layout.tsx
@@ -1,15 +1,13 @@
 import Providers from '@/providers'
 import { FC, ReactNode } from 'react'
-import Footer from '../molecules/footer'
-import Header from '../molecules/header'
 
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <Providers>
       <div className="flex w-screen h-screen flex-col p-8 gap-16">
-        <Header />
+        {/* <Header /> */}
         <main className="flex-grow w-full h-full max-h-full overflow-hidden">{children}</main>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </Providers>
   )
